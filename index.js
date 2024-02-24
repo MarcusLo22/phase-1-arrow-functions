f3c6fe0a9837dd66f3f23e39e12ee379e81f0d87
Marcus Lo <marcusklo1010@gmail.com>
2024-02-24 09:02:05 -0700

    Arrow functions complete

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (parameter1, parameter2) => {
+    console.log(`Dividing ${parameter1}`);
+    console.log(`Dividing ${parameter2}`);
+    return parameter1 / parameter2;
+}
+divide(2000,100);
+
+const square = (number) => number * number;
+square(10)
+
+const add = (number1, number2) => number1 + number2;
+add(3,4); 
